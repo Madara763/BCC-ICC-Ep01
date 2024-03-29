8aea19033d10e9853e2315b5c3647790a5c1694d
Madara 763 <clashroyalemadara@gmail.com>
2024-03-29 20:10:29 -0300

    inicio_lib

---
+++ EliminacaoGauss.c
@@ -0,0 +1,9 @@
+#include "EliminacaoGauss.h"
+
+/*Matriz M, Vetor B de termos independentes, Tamnaho da matriz n*/
+void eliminacaoGauss(double **M, double *B, unsigned int n){
+
+    for(int i=0; i<n; i++){
+        unsigned int pivo=
+    }
+}
---
+++ EliminacaoGauss.h
@@ -0,0 +1,13 @@
+/*
+Biblioteca que implementa o algoritmo de
+Eliminação de Gauss com pivoteamento parcial
+Davi Lazzarin - 29/03/2024
+*/
+
+#ifndef _ELIMINA_GAUSS_
+#define _ELIMINA_GAUSS_
+
+/*Matriz M, Vetor B de termos independentes, Tamnaho da matriz n*/
+void eliminacaoGauss(double **M, double *B, unsigned int n);
+
+#endif
